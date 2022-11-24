09791e9fcee5e09dd5b6e29bf2753cf54ecb8696
Oksana <ksenia79460@gmail.com>
2022-11-24 16:21:55 +0000

    delete task, toggle classes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,50 @@
-import Header from './components/Header';
+import { useState } from "react";
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      message: "Hi my friend",
+      time: "Monday 11 November",
+      reminder: true,
+    },
+    {
+      id: 2,
+      message: "Hi my friend dldldjjdj",
+      time: "Monday 14 November",
+      reminder: true,
+    },
+    {
+      id: 3,
+      message: "Hi my friend bxbxvxvx",
+      time: "Monday 15 November",
+      reminder: true,
+    },
+  ]);
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
+
   return (
     <div className="container">
-     <Header />
+      <Header />
+
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        "No tasks to show"
+      )}
     </div>
   );
 }
---
+++ src/components/Task.js
@@ -0,0 +1,21 @@
+import { FaTimes } from "react-icons/fa";
+
+const Task = ({ task, onDelete, onToggle }) => {
+  return (
+    <div
+      className={`task ${task.reminder ? "reminder" : ""}`}
+      onDoubleClick={() => onToggle(task.id)}
+    >
+      <h3>
+        {task.message}{" "}
+        <FaTimes
+          style={{ color: "darkred", cursor: "pointer" }}
+          onClick={() => onDelete(task.id)}
+        />
+      </h3>
+      <p>{task.time}</p>
+    </div>
+  );
+};
+
+export default Task;
